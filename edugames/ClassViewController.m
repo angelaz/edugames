00f74cf02842c2a8eec23e715a00b9aa8f8f5b53
Angela Zhang <angelafzhang@gmail.com>
2014-02-15 03:04:22 -0500

    Tableview now occupies half the screen

--- edugames/ClassViewController.m
+++ edugames/ClassViewController.m
@@ -28,13 +28,10 @@
 
 - (void)loadView
 {
-    /*CGRect frame = [[UIScreen mainScreen] applicationFrame];
-    NSLog(@"%@", frame);
-    CGRect leftTableView = CGRectMake(0, 0, frame.size.width/2, frame.size.height);
-    UITableView *tableView = [[UITableView alloc] initWithFrame:leftTableView style:UITableViewStylePlain];*/
-    
-    //TODO: Make the above commented out code work so that we have a tableview that only takes up half the screen
-    UITableView *tableView = [[UITableView alloc] initWithFrame:[[UIScreen mainScreen] applicationFrame] style:UITableViewStylePlain];
+    [super loadView];
+    CGRect frame = [[UIScreen mainScreen] applicationFrame];
+    CGRect leftTableView = CGRectMake(0, 0, (frame.size.width-84)/2, frame.size.height);
+    UITableView *tableView = [[UITableView alloc] initWithFrame:leftTableView style:UITableViewStylePlain];
     
     tableView.autoresizingMask = UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth;
     tableView.delegate = self;
