8252e19b84376a17682b7166d4d1df1584d2bf93
Lucy Guo <lguo@andrew.cmu.edu>
2014-02-15 02:21:29 -0500

    added teacherprofile class

---
+++ TeacherProfileViewController.m
@@ -0,0 +1,38 @@
+//
+//  TeacherProfileViewController.m
+//  edugames
+//
+//  Created by Lucy Guo on 2/15/14.
+//  Copyright (c) 2014 Angela Zhang, Lucy Guo, Ivan Wang, Gregory Rose. All rights reserved.
+//
+
+#import "TeacherProfileViewController.h"
+
+@interface TeacherProfileViewController ()
+
+@end
+
+@implementation TeacherProfileViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
