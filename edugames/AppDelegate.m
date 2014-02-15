8fba961d19d1596cbe8dd67e648f8b22db9313d7
Angela Zhang <angelafzhang@gmail.com>
2014-02-14 23:06:43 -0500

    Started implementing sidebar for nav.

--- edugames/AppDelegate.m
+++ edugames/AppDelegate.m
@@ -8,29 +8,74 @@
 
 #import "AppDelegate.h"
 #import "LoginViewController.h"
+#import "CKSideBarController.h"
+#import "CKTestViewController.h"
+
+@interface AppDelegate () <CKSideBarControllerDelegate>
+
+@property(nonatomic) CKSideBarController *barController;
+@property(nonatomic) UINavigationController *controller2;
+@property(nonatomic) UINavigationController *controller1;
+@property(nonatomic) UINavigationController *controller3;
+
+@end
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [[UINavigationBar appearance] setTintColor:[UIColor colorWithHue:0.6 saturation:0.55 brightness: 0.69 alpha:1.0]];
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    UISplitViewController *splitViewController = (UISplitViewController *)self.window.rootViewController;
-    UINavigationController *navigationController = [splitViewController.viewControllers lastObject];
-    splitViewController.delegate = (id)navigationController.topViewController;
+    self.window.backgroundColor = [UIColor blackColor];
+    [self.window makeKeyAndVisible];
     
-    //setting up login view controller
-    LoginViewController *loginViewController = [[LoginViewController alloc] init];
+    self.controller1 = [self navControllerWithTitle:@"Tweets" barImage:[UIImage imageNamed:@"compose.png"]];
+    self.controller2 = [self navControllerWithTitle:@"Connect" barImage:[UIImage imageNamed:@"compose.png"]];
+    self.controller3 = [self navControllerWithTitle:@"Can't Touch" barImage:[UIImage imageNamed:@"search.png"]];
     
-    self.loginViewController = loginViewController;    
-    self.window.rootViewController = loginViewController;
+    UIViewController *detachedController = [[UIViewController alloc] init];
     
-
+    self.barController = [[CKSideBarController alloc] init];
+    self.barController.delegate = self;
+    self.barController.viewControllers = @[
+                                           self.controller1,
+                                           self.controller2,
+                                           self.controller3
+                                           ];
     
-    //Need to set up Facebook login and Parse
-    
+    self.window.rootViewController = self.barController;
     return YES;
 }
-							
+
+//Side bar controller stuff
+BOOL shouldAlternate = YES;
+
+- (UINavigationController *)navControllerWithTitle:(NSString *)title barImage:(UIImage *)image {
+    CKTestViewController *controller = [[CKTestViewController alloc] init];
+    controller.title = title;
+    controller.view.backgroundColor = [UIColor whiteColor];
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:controller];
+    //if (shouldAlternate)
+        navController.sideBarItem.title = title;
+    //if (image)
+        navController.sideBarItem.image = image;
+    navController.sideBarItem.isGlowing = shouldAlternate;
+    
+    //shouldAlternate = !shouldAlternate;
+    
+    return navController;
+}
+
+- (void)updateViewControllers {
+    self.barController.viewControllers = @[
+                                           self.controller1,
+                                           self.controller3,
+                                           self.controller2
+                                           ];
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
