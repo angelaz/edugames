ae9a956be0eb0b181469f6715acd1f472cf162ee
Gregory Rose <greg@leanplum.com>
2014-02-15 20:03:29 -0500

    calculating hexagons

--- edugames/ConquerScene.m
+++ edugames/ConquerScene.m
@@ -10,12 +10,29 @@
 
 @implementation ConquerScene
 {
-    bool hexagons[5][5];
+    NSDictionary* gameState;
+    // map of hexagons
+    // player locations
 }
 
-CGPoint coordToPointWith(int i, int j)
+int geti(int u)
 {
-    int startx,starty;
+    return u / 5;
+}
+
+int getj(int u)
+{
+    return u % 5;
+}
+
+int pack(int i, int j)
+{
+    return 5*i + j;
+}
+
+CGPoint coordToPoint(int i, int j)
+{
+    int startx, starty;
     if (i == 0)
     {
         startx = 80;
@@ -23,10 +40,12 @@ CGPoint coordToPointWith(int i, int j)
     } else
     {
         startx = 80 + 100*(i-1);
-        starty = 455 + 100*(i-1);
+        starty = 455 - 60*(i-1);
     }
     int x = startx + j*100;
     int y = starty + j*60;
+    
+    NSLog(@"i: %d, j: %d, x: %d, y: %d", i, j, x, y);
     
     return CGPointMake(x,y);
 }
@@ -37,16 +56,6 @@ CGPoint coordToPointWith(int i, int j)
         
 //        self.backgroundColor = [SKColor colorWithRed:0.15 green:0.15 blue:0.3 alpha:1.0];
         
-        for (int i = 0; i < 5; i++) {
-            int limit = 5;
-            if (i == 0 || i == 4)
-                limit = 4;
-            
-            int blocked = arc4random() % limit;
-            for (int j = 0; j < limit; j++) {
-                hexagons[i][j] = (blocked == j);
-            }
-        }
         
         SKSpriteNode *background = [SKSpriteNode spriteNodeWithImageNamed:@"game-bg-2.png"];
         background.position = CGPointMake(CGRectGetMidX(self.frame),CGRectGetMidY(self.frame));
@@ -60,6 +69,28 @@ CGPoint coordToPointWith(int i, int j)
                                        CGRectGetMidY(self.frame) + 250);
         
         [self addChild:myLabel];
+        
+        
+        NSMutableDictionary* points = [[NSMutableDictionary alloc] init];
+        NSMutableDictionary* hexagons = [[NSMutableDictionary alloc] init];
+        
+        for (int i = 0; i < 5; i++) {
+            int limit = 5;
+            if (i == 0 || i == 4)
+                limit = 4;
+            
+            int blocked = arc4random() % limit;
+            for (int j = 0; j < limit; j++) {
+                NSNumber* u = [NSNumber numberWithInt:pack(i, j)];
+                points[u] = [NSNumber numberWithBool:(blocked == j)];
+                
+                SKSpriteNode *hexagon = [SKSpriteNode spriteNodeWithImageNamed:@"close-button.png"];
+                hexagon.position = coordToPoint(i, j);
+                //hexagon.isHidden = (blocked == j);
+                [self addChild:hexagon];
+                hexagons[u] = hexagon;
+            }
+        }
     }
     return self;
 }
